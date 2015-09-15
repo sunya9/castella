92d0f97f2e0b4a76fbd9a016bdec17a525011f00
sunya <mugen.Xyz@gmail.com>
2015-09-16 04:55:16 +0900

    use gulp-cssnext.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,31 @@
-import pkg from 'package.json';
-import autoprefixer from 'autoprefixer';
-import csswring from 'csswring';
+import path from 'path';
+import mixins from 'postcss-mixins';
+import nested from 'postcss-nested';
+import extend from 'postcss-extend';
+
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
+var pkg =  require('./package.json');
 var $ = gulpLoadPlugins();
 
 const CSS_SRC_DIR = './css/';
 
 const DIST_DIR = './dist/';
-const CSS_DIST_DIR = DIST_DIR + 'css/';
-const JS_DIST_DIR = DIST_DIR + 'js/';
+const CSS_DIST_DIR = path.join(DIST_DIR, 'css/');
+const JS_DIST_DIR = path.join(DIST_DIR, 'js/');
 
 gulp.task('css', () => {
-  var processors = [
-    autoprefixer({broswers: ['last 1 version', 'ie9']}),
-    csswring()
-  ];
-  return gulp.src(CSS_SRC_DIR + '**/*.css')
-    .pipe(processors)
-    .pipe(gulp.dest(CSS_DIST_DIR + pkg.name + '.css'))
+  return gulp.src(CSS_SRC_DIR + '**/' + pkg.name + '.css')
+    .pipe($.cssnext({
+      compress: process.env.NODE_ENV === 'production',
+      plugins: [
+        nested,
+        mixins,
+        extend,
+      ]
+    }))
+    .pipe(gulp.dest(CSS_DIST_DIR))
     .pipe($.livereload());
 });
 
@@ -27,3 +33,7 @@ gulp.task('watch', () => {
   $.livereload.listen();
   return gulp.watch(CSS_SRC_DIR + '**/*.css', ['css']);
 });
+
+gulp.task('build', ['css']);
+
+gulp.task('default', ['build']);
