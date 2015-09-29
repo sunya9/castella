bb6efe517bda536a904258e1819e602c726642a9
sunya <mugen.Xyz@gmail.com>
2015-09-30 03:55:42 +0900

    Add browserify.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,17 +4,23 @@ import nested from 'postcss-nested';
 import extend from 'postcss-sass-extend';
 import extend2 from 'postcss-extend';
 import repeat from 'postcss-for';
-import simpleVars from 'postcss-simple-vars'
-import each from 'postcss-each'
+import simpleVars from 'postcss-simple-vars';
+import each from 'postcss-each';
 import cssMqpacker from 'css-mqpacker';
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import csswring from 'csswring';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
 
 var pkg =  require('./package.json');
 var $ = gulpLoadPlugins();
 
-const CSS_SRC_DIR = './src/css/';
+const SRC_DIR = './src/';
+const CSS_SRC_DIR = path.join(SRC_DIR, 'css/');
+const JS_SRC_DIR = path.join(SRC_DIR, 'js/');
 
 const DIST_DIR = './dist/';
 const CSS_DIST_DIR = path.join(DIST_DIR, 'css/');
@@ -46,9 +52,27 @@ gulp.task('css', () => {
     .pipe(gulp.dest(CSS_DIST_DIR));
 });
 
+gulp.task('js', () => {
+  var b = browserify({
+    entries: JS_SRC_DIR + pkg.name + '.js',
+    debug: process.env.NODE_ENV !== 'production',
+    transform: babelify
+  });
+  return b.bundle()
+  .pipe(source(pkg.name + '.js'))
+  .pipe(gulp.dest(JS_DIST_DIR))
+  .pipe(buffer())
+  .pipe($.uglify())
+  .pipe($.rename({
+    extname: '.min.js'
+  }))
+  .pipe(gulp.dest(JS_DIST_DIR));
+});
+
 gulp.task('watch', () => {
   $.livereload.listen();
-  return gulp.watch(CSS_SRC_DIR + '**/*.css', ['css']);
+  gulp.watch(CSS_SRC_DIR + '**/*.css', ['css']);
+  return gulp.watch(JS_SRC_DIR + '**/*.js', ['js']);
 });
 
 gulp.task('build', ['css']);
